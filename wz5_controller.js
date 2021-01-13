b522081d0d326ec5a1b7e5e11c9c40ac6d8f74b1
SandrRu <77043365+SandrRu@users.noreply.github.com>
2021-01-13 12:18:42 +0300

    Try to add system_mode change
    I cant understand how add custom expose ......

--- wz5_controller.js
+++ wz5_controller.js
@@ -1,8 +1,5 @@
 /*
-* Created by Sandr.ru at 08-01-2021
-* MQTT Example command 
-* zigbee2mqtt/PWM_PA2/l1/set
-* {"brightness_l1":13, "transition":3} 
+* Created by Sandr.ru at 12-01-2021
 **/
 const fz = {...require('zigbee-herdsman-converters/converters/fromZigbee'), legacy: require('zigbee-herdsman-converters/lib/legacy').fromZigbee};
 const { tuyaGetDataValue } = require('zigbee-herdsman-converters/lib/legacy');
@@ -13,9 +10,34 @@ const e = exposes.presets;
 const tuya = require('zigbee-herdsman-converters/lib/tuya');
 
 const wz5dataPoints = {
-    wz5_state: 2,
-}
+    mode: 2,  //enum [ 0-255 ] {"status":0,"transid":3,"dp":2,"datatype":4,"fn":0,"data":{"type":"Buffer","data":[0]}}
+    color_temp: 4, //value [ 4 byte ] {"status":0,"transid":5,"dp":4,"datatype":2,"fn":0,"data":{"type":"Buffer","data":[0,0,0,171]}}
+    color: 7, //value [ 4 byte ] {"status":0,"transid":6,"dp":7,"datatype":2,"fn":0,"data":{"type":"Buffer","data":[0,0,0,0]}}
+};
+/*
+const wz5exposes = {
+    class Wz5light extends Base {
+        constructor() {
+            super();
+            this.type = 'wz5light';
+            this.features = [];
+        }
 
+        withSystemMode(modes, access=a.ALL) {
+            assert(!this.endpoint, 'Cannot add feature after adding endpoint');
+            const allowed = ['white', 'rgb'];
+            modes.forEach((m) => assert(allowed.includes(m)));
+            this.features.push(new Enum('system_mode', access, modes).withDescription('Mode of this device'));
+            return this;
+        }
+    }
+    module.exports = {
+        access,
+        wz5light: () => new Wz5light(),
+        
+    }
+}
+*/
 const fromZigbee = {
     wz5_controller: {
         cluster: 'manuSpecificTuya',
@@ -25,34 +47,50 @@ const fromZigbee = {
             const value = tuya.getDataValue(msg.data.datatype, msg.data.data); // This function will take of converting the data to proper JS type
 
             switch (dp) {
-            case tuya.dataPoints.state: // DPID that we added to common
-                return { state: value ? 'ON' : 'OFF' }; 
-            case tuya.dataPoints.mode:
-                switch (value) {
-                    default:
-                        meta.logger.warn('WZ5: ' +
-                            `preset ${value} is not recognized.`);
+                case tuya.dataPoints.state: // DPID that we added to common
+                    return { state: value ? 'ON' : 'OFF' }; 
+                case tuya.dataPoints.dimmerLevel: 
+                    return {brightness: value}; //(value / 10).toFixed(1)
+                case tuya.dataPoints.mode: 
+                    switch (value) {
+                        case 0: // white
+                            return {system_mode: 'white'};
+                        case 1: // rgb
+                            return {system_mode: 'rgb'};
+                        default:
+                            meta.logger.warn('WZ5:mode ' + `preset ${value} is not recognized.`);
                         break;
-                }
-                break;            
-            case wz5dataPoints:
-                switch (value) {
-                   default:
-                    meta.logger.warn('WZ5: ' +
-                        `preset ${value} is not recognized.`);
+                    }
                     break;
-                }
-                break;            
-            case tuya.dataPoints.dimmerLevel: // DPID that we added to common
-                return {brightness: value}; //(value / 10).toFixed(1)
-            default:
-                meta.logger.warn(`WZ5: NOT RECOGNIZED DP #${dp} with data ${JSON.stringify(msg.data)}`); 
+                case wz5dataPoints.color_temp: 
+                case wz5dataPoints.color: // DPID that we added to common
+                    meta.logger.warn(`WZ5: #${dp} with data ${JSON.stringify(msg.data)}`); 
+                default:
+                    meta.logger.warn(`WZ5: NOT RECOGNIZED DP #${dp} with data ${JSON.stringify(msg.data)}`); 
             }
         },
     },   
 };
 
 const toZigbee = {
+    system_mode: {
+        key: ['system_mode'],
+        convertSet: async (entity, key, value, meta) => {
+            switch (value) {
+                case 'white':
+                    await tuya.sendDataPointBool(entity, tuya.dataPoints.state, true);
+                    await utils.sleep(500);
+                    await tuya.sendDataPointEnum(entity, tuya.dataPoints.mode, 0 /* white */);
+                    break;
+                case 'rgb':
+                    await tuya.sendDataPointBool(entity, tuya.dataPoints.state, true);
+                    await utils.sleep(500);
+                    await tuya.sendDataPointEnum(entity, tuya.dataPoints.mode, 1 /* rgb */);
+                    break;
+                break;
+            }
+        },
+    },
 };
 
 const bind = async (endpoint, target, clusters) => {
@@ -65,14 +103,6 @@ const devices = [
     {
         fingerprint: [ 
             {
-/*
-Zigbee2MQTT:warn  2021-01-12 18:06:50: Unhandled DP #1: {"status":0,"transid":2,"dp":1,"datatype":1,"fn":0,"data":{"type":"Buffer","data":[1]}}
-Zigbee2MQTT:warn  2021-01-12 18:06:50: Unhandled DP #2: {"status":0,"transid":3,"dp":2,"datatype":4,"fn":0,"data":{"type":"Buffer","data":[0]}}
-Zigbee2MQTT:warn  2021-01-12 18:06:50: Unhandled DP #3: {"status":0,"transid":4,"dp":3,"datatype":2,"fn":0,"data":{"type":"Buffer","data":[0,0,3,232]}}
-Zigbee2MQTT:warn  2021-01-12 18:06:50: Unhandled DP #4: {"status":0,"transid":5,"dp":4,"datatype":2,"fn":0,"data":{"type":"Buffer","data":[0,0,2,222]}}
-Zigbee2MQTT:warn  2021-01-12 18:06:50: Unhandled DP #7: {"status":0,"transid":6,"dp":7,"datatype":2,"fn":0,"data":{"type":"Buffer","data":[0,0,0,0]}}
-
-*/
                 modelID: 'TS0601', // You may need to add \u0000 at the end of the name in some cases
                 manufacturerName: '_TZE200_mde0utnv' 
             },
@@ -91,12 +121,12 @@ Zigbee2MQTT:warn  2021-01-12 18:06:50: Unhandled DP #7: {"status":0,"transid":6,
             tz.tuya_dimmer_state,
             tz.tuya_dimmer_level,
             tz.on_off, 
-            tz.tuya_led_control, 
             tz.tuya_led_controller,
-            tz.tuya_data_point_test,
+            toZigbee.system_mode
         ],
         exposes: [
-            e.light_brightness()
+            e.light_brightness(),
+            //wz5exposes.wz5light().withSystemMode(['white', 'rgb']),
             //e.light_brightness_colortemp_colorhs()
         ],
 
